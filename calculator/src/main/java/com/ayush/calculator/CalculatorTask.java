2980d7c5deff1786cdd2b711fb366ac0fc9a8f85
Ayushakv <vermaayushsal@gmail.com>
2021-02-01 23:10:20 +0530

    Third

---
+++ calculator/src/main/java/com/ayush/calculator/CalculatorTask.java
@@ -0,0 +1,22 @@
+package com.ayush.calculator;
+
+public class CalculatorTask {
+	double a,b;
+	double add(double a,double b)
+	{
+		return a+b;
+	}
+	double subtract(double a,double b)
+	{
+		return a-b;
+	}
+	double multiply(double a,double b)
+	{
+		return a*b;
+	}
+	double divide(double a,double b)
+	{
+		return a/b;
+	}
+	
+}
